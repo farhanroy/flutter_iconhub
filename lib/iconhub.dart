e34431e47863003b9cd6e5a12568010a3bf6d439
farhanroy <farhanroy120@gmail.com>
2021-07-02 07:29:38 +0700

    :hammer: adding icon

--- lib/iconhub.dart
+++ lib/iconhub.dart
@@ -1,7 +1,4 @@
 library iconhub;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export './src/iconhub.dart';
+export './src/iconhubs.dart';
